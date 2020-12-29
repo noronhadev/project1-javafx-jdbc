5989e10a4b4058aa41ab192b70582952b9b524db
Guilherme Noronha <noronhadev@gmail.com>
2020-12-29 16:52:46 -0300

    Validation exception

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -5,6 +5,7 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
+
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
